000d760c5527cbee04f28e1f6c4a858879945dea
OXM12 <104848086+OXM12@users.noreply.github.com>
2022-10-02 21:22:58 +0500

    first con

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "5629440451:AAF1pAYM0vlTasCCR6nwE6f8sh2GNvgJYKE"
+APP_URL = "https://tesdbddb1.herokuapp.com/" + BOT_TOKEN
+# DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
