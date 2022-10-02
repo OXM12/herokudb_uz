78baa20d3a1b23582be080ea04ad6c19a067f173
OXM12 <104848086+OXM12@users.noreply.github.com>
2022-10-02 21:52:31 +0500

    dfddf

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def start(message):
     result = db_object.fetchone()
 
     if not result:
-        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (?, ?, ?)", (user_id, username, 0))
         db_connection.commit()
 
     update_messages_count(user_id)
