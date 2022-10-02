3a7d5951ff27b10f60eac5a169b943d602eb68b8
OXM12 <104848086+OXM12@users.noreply.github.com>
2022-10-02 21:47:40 +0500

    first db connect

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5629440451:AAF1pAYM0vlTasCCR6nwE6f8sh2GNvgJYKE"
 APP_URL = "https://tesdbddb1.herokuapp.com/" + BOT_TOKEN
-# DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
+DB_URI = "postgres://xsjcxyxabirpno:2b7e443383cf2195e31c74cedb2923c6bba18d86af54e74bed9e9a15c36e1651@ec2-54-220-255-121.eu-west-1.compute.amazonaws.com:5432/d10cfr9am588l0"
